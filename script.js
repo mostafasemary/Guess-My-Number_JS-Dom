d3624f4dd2fd71696c37b000f1e2727870ccd3bc
Mostafa Semary <mostafasemary7@gmail.com>
2025-08-30 03:26:29 +0200

    implementing the game logic

--- script.js
+++ script.js
@@ -11,11 +11,35 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number! ❌';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Coreect Number! 🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high! 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game! 💔';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low! 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game! 💔';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
