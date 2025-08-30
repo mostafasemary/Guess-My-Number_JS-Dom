01d2ee3cdc97882106e2820e08296d9b88ae7196
Mostafa Semary <mostafasemary7@gmail.com>
2025-08-30 04:46:08 +0200

    solve coding challenge#1

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+// document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
@@ -26,7 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Coreect Number! 🎉';
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
     //When guess is too high
   } else if (guess > secretNumber) {
@@ -49,4 +49,26 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+// Coding challenge #1
+/*
+Implement a game rest functionality, so that the player can make new guess
+Here is How:
+//1.Select the element with the 'again' class and attach a click event handler
+
+//2. In the handler function, restore initial values of the score and secretNumber varaibles
+
+//3. Restore the initial conditions of the message,number, score and guess input field
+
+//4. Also restore the original background color (#222) and number width (15rem)
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
