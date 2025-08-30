f54bbd2e08cdbe288779766f7863286faed35cc3
Mostafa Semary <mostafasemary7@gmail.com>
2025-08-30 04:03:50 +0200

    Manipulating css styles

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
+  // When is this not input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number! ❌';
   } else if (guess === secretNumber) {
+    // When Player Wins
     document.querySelector('.message').textContent = 'Coreect Number! 🎉';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high! 📈';
@@ -32,6 +38,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You Lost the game! 💔';
       document.querySelector('.score').textContent = 0;
     }
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low! 📉';
